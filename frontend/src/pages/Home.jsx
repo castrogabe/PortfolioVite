63e16ba8bf73755383cc7b9d511861e0132e4e8f
Gabriel Castro <43317756+castrogabe@users.noreply.github.com>
2025-08-29 07:14:22 -0700

    2nd Commit-Vite setup Portfolio frontend, basic pages, footer, header

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,7 @@
+// rcf
+
+import React from 'react';
+
+export default function Home() {
+  return <div>Home</div>;
+}
---
+++ frontend/src/pages/Portfolio.jsx
@@ -0,0 +1,6 @@
+// rfc
+import React from 'react';
+
+export default function Portfolio() {
+  return <div>Portfolio</div>;
+}
